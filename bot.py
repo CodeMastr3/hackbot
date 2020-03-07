13cf2a53e1fedd55624be796c13fd7794d19b55e
Cody Evans <1123code@gmail.com>
2020-03-07 10:14:05 -0800

    Removed token from repo

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import discord
+#import emojiRole.py
+import token1
+
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!')
@@ -11,12 +14,39 @@ async def ping(ctx):
 async def logout(ctx):
     await bot.logout()
 
-watched_messages = {}
+watched_message = {}
 emojiList1 = {}
 
-emojiList1['/SMILYFACE'] = roleID
+emojiList1['afc96e77efee1190e1fbe3cc69f149f8'] = '<@&685902355404947535>'
+emojiList1['df854ca9a022bf3b5fe42ded8725e1bc'] = '<@&685891891006275604>'
 
-watched_message[message1ID] = emojiList1
+watched_message['22'] = emojiList1
+
+async def manage_reaction(self, reaction, user, added: bool):
+    if not reaction.message.id in self.watched_messages:
+        return
+
+    messageID = reaction.message.id
+    mapping = self.watched_messages[messageID]
+    if not reaction.emoji in mapping:
+        # reaction.emoji is str if normal emoji or ID if custom, but we use both as keys in mapping
+        return
+
+    member = discord.utils.get(reaction.message.server.members, id=user.id)
+    role = discord.utils.get(reaction.message.server.roles, id=mapping[reaction.emoji])
+
+    if added:
+        await bot.add_roles(member, role)
+    else:
+        await bot.remove_roles(member, role)
+
+@bot.event
+async def on_reaction_add(self, reaction, user):
+    await self.manage_reactions(reaction, user, True)
+
+@bot.event
+async def on_reaction_remove(self, reaction, user):
+    await self.manage_reactions(reaction, user, False)
 
 
 
@@ -43,7 +73,5 @@ watched_message[message1ID] = emojiList1
 
 
 
-
-
-bot.run('Njg1ODkyNjI2NzI5MjcxMzk1.XmPSRg.UJJTAi_Cv3e48IuAgsRpU8kYjes')
+bot.run(token1.stringToken())
 
